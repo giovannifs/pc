0ba1d7baf941d1a976a0f9dc5d770f8254ca0cbb
giovannifs <giovanni@lsd.ufcg.edu.br>
2024-08-27 16:52:12 -0300

    Uploading first version of script.

---
+++ concurrent-broker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+
+SERVER_ADRESS=150.165.85.31
+SERVER_USER="ubuntu"
+PUBLIC_KEY_PATH="$BASE_DIR/key/public_key.pem"
+SSH_KEY="$BASE_DIR/key/id_concurrent_rsa"
+
+FILE_PATH=$1
+FILE_NAME=$(basename $FILE_PATH .txt)
+
+echo $BASE_DIR
+echo $FILE_NAME
+
+#ls -lh "~/.ssh/"
+
+# Encrypting your answer file using the Public Key
+
+echo openssl rsautl -encrypt -inkey $PUBLIC_KEY_PATH -pubin -in $FILE_PATH -out "$BASE_DIR/$FILE_NAME"-encrypted.bin
+
+# Sending your encrypted answer file to the server
+
+echo scp -i $SSH_KEY "$BASE_DIR/$FILE_NAME"-encrypted.bin" $SERVER_USER@$SERVER_ADRESS:/home/$SERVER_USER/concurrent-answers/24.1/prova2/
